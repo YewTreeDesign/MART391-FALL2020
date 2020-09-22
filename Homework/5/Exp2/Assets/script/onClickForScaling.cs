407bd8828330d6a957b19e1c9c6aa806e9b0a950
Charlene Mundel <35701097+YewTreeDesign@users.noreply.github.com>
2020-09-21 21:10:29 -0600

    5

---
+++ Homework/5/Exp2/Assets/script/onClickForScaling.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class onClickForScaling : MonoBehaviour {
+	void OnMouseDown() {
+		CSharpscaling.ScaleTransform = this.transform;
+	}
+}
